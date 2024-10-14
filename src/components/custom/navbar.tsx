b090b78d948007b21b7ddff3c2c280e43bd08983
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-10-14 19:09:42 +0900

    fix Navbar.

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -4,19 +4,17 @@ import { ThemeToggle } from "./theme-toggle";
 
 const Navbar: React.FC = () => {
   return (
-    <>
-      <div className="bg-background sticky top-0 w-dvw px-3 py-3 justify-between flex flex-row items-center z-30">
-        <div>title</div>
-        <div className="flex flex-row items-center gap-2">
-          <Button asChild>
-            <ThemeToggle />
-          </Button>
-          <Button className="py-2 px-3 h-fit font-normal" asChild>
-            <Link href="/login">Login</Link>
-          </Button>
-        </div>
+    <div className="bg-background sticky top-0 w-dvw px-3 py-3 justify-between flex flex-row items-center max-h-20">
+      <div>title</div>
+      <div className="flex flex-row items-center gap-2">
+        <Button asChild>
+          <ThemeToggle />
+        </Button>
+        <Button className="py-2 px-3 h-fit font-normal" asChild>
+          <Link href="/login">Login</Link>
+        </Button>
       </div>
-    </>
+    </div>
   );
 };
 Navbar.displayName = "Navbar";
