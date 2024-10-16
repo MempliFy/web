4c0e2030795d9b429c145ac786915438b29173b9
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-10-16 09:29:29 +0900

    Add Multimodal input in chat.

--- src/components/custom/chat.tsx
+++ src/components/custom/chat.tsx
@@ -1,4 +1,5 @@
 import { Message as PreviewMessage } from "@/components/custom/message";
+import { MultimodalInput } from "./multimodal-input";
 
 type Props = {
   initialMessages: Array<Message>;
@@ -8,8 +9,10 @@ export const Chat: React.FC<Props> = () => {
   return (
     <div className="flex flex-col justify-center bg-background h-[calc(100vh-theme(spacing.navbar))]">
       <div className="flex-1 overflow-y-auto"></div>
-      <div className="px-2 py-2">
-        <form className="flex flex-col justify-center items-center"></form>
+      <div className="p-2">
+        <form className="flex flex-col justify-center items-center">
+          <MultimodalInput />
+        </form>
       </div>
     </div>
   );
