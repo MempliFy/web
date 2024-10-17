74fd390b65ecace071b6fbb57335aca7038ece4a
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-10-17 13:22:45 +0900

    Add Sidebar template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Provider } from "jotai";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/custom/theme-provider";
 import { Navbar } from "@/components/custom/navbar";
+import { Sidebar } from "@/components/custom/sidebar";
 
 export const metadata: Metadata = {
   title: "MempliFy Web App",
@@ -26,8 +27,13 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Toaster position="top-center" />
-            <Navbar />
-            <div className="p-3">{children}</div>
+            <div className="flex w-dvw">
+              <Sidebar />
+              <div className="flex-grow">
+                <Navbar />
+                <div className="p-3">{children}</div>
+              </div>
+            </div>
           </ThemeProvider>
         </Provider>
       </body>
