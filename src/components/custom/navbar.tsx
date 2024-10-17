ff88e952e185266c92719c345a056e1310436b8f
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-10-17 13:23:07 +0900

    fix width in navbar.

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { ThemeToggle } from "./theme-toggle";
+import { HamBurgerIcon } from "./icons";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="bg-background sticky top-0 w-dvw px-3 py-3 justify-between flex flex-row items-center max-h-20">
-      <div>title</div>
+    <div className="bg-background sticky top-0 w-full px-3 py-3 justify-between flex flex-row items-center max-h-20">
+      <HamBurgerIcon size={24} />
+      <span>MempliFy</span>
       <div className="flex flex-row items-center gap-2">
         <Button asChild>
           <ThemeToggle />
